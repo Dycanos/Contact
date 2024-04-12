e1a6032ce8703a53a79408bede0b2712674cb62d
Dycanos <ljacques54450@gmail.com>
2024-04-12 11:56:57 +0200

    🎉 - Start Contact project

---
+++ app/src/main/java/com/example/contact/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.contact
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.contact.ui.theme.ContactTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ContactTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ContactApp()
+                }
+            }
+        }
+    }
+}
